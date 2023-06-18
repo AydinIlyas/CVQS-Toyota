243a52da10c66ea78ea4609d7d919e6df5b2a568
Ilyas Aydin <ilyasaydin55sa@gmail.com>
2023-06-18 10:27:53 +0300

    Fixed Test

--- errorLogIn-service/src/test/java/com/toyota/errorloginservice/ErrorLogInServiceApplicationTests.java
+++ errorLogIn-service/src/test/java/com/toyota/errorloginservice/ErrorLogInServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.toyota.errorloginservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ErrorLogInServiceApplicationTests {
 
     @Test
