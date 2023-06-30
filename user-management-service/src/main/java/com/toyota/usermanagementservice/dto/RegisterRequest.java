2fe77924f168504d2dd5f7faee62d24db65681ae
Ilyas Aydin <ilyasaydin55sa@gmail.com>
2023-06-30 11:06:57 +0300

    Added validation constraints for user-management-service dtos

--- user-management-service/src/main/java/com/toyota/usermanagementservice/dto/RegisterRequest.java
+++ user-management-service/src/main/java/com/toyota/usermanagementservice/dto/RegisterRequest.java
@@ -1,19 +1,26 @@
 package com.toyota.usermanagementservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.Set;
 
 /**
  * Class for sending request to save user in verification service
  */
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "Username must not be blank")
     private String username;
+    @NotBlank(message = "Password must not be blank")
+    @Size(min=8)
     private String password;
+    @NotNull(message = "Roles must not be null")
+    @Size(min = 1,message = "User must have at least 1 role!")
     private Set<String> roles;
 }
--- user-management-service/src/main/java/com/toyota/usermanagementservice/dto/UserDTO.java
+++ user-management-service/src/main/java/com/toyota/usermanagementservice/dto/UserDTO.java
@@ -4,6 +4,7 @@ package com.toyota.usermanagementservice.dto;
 import com.toyota.usermanagementservice.domain.Gender;
 import com.toyota.usermanagementservice.domain.Role;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -18,18 +19,20 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
-    @NotNull
+    @NotBlank(message = "Firstname must not be blank")
     private String firstname;
-    @NotNull
+    @NotBlank(message="Lastname must not be blank")
     private String lastname;
-    @NotNull
+    @NotBlank(message = "Username must not be blank")
     private String username;
-    @Email
-    @NotNull
+    @Email(message = "It must be a valid email")
+    @NotBlank(message = "Email must no be blank")
     private String email;
-    @NotNull
-    @Size(min = 5)
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 8,message = "Password must have at least 8 characters")
     private String password;
+    @Size(min = 1,message = "User must have at least one role")
+    @NotNull(message = "Role must not be null")
     private Set<Role> role;
     private Gender gender;
 }
